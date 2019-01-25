b62f46a729a9ef5f61694c68eeb0a3fc8b067410
Scott <scottcampbell.boise@gmail.com>
2019-01-24 17:32:46 -0800

    Updated program files

--- PA 1 - Yahtzee/src/Yahtzee.java
+++ PA 1 - Yahtzee/src/Yahtzee.java
@@ -2,7 +2,7 @@
 /*
  * Homework #1
  * Due Date: Tuesday, January 29th
- * Names: Scott Campbell (scampbell7)
+ * Names: Scott Campbell & Arron Cushing
  */
 
 import java.util.Scanner;
@@ -13,19 +13,19 @@ public class Yahtzee {
 	{
 		new Yahtzee();
 	}
-	
+
 	public Yahtzee()
 	{
 		final int DICE_IN_PLAY = 5;
 		int[] hand = new int[DICE_IN_PLAY];
 		char playAgain = 'y';
 		Scanner scnr = new Scanner(System.in);
-		
+
 		while(playAgain == 'y')
 		{
 			String keep = "nnnnn";
 			int turn = 1;
-			
+
 			while(turn < 4 && !keep.equals("yyyyy"))
 			{
 				for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
@@ -35,22 +35,22 @@ public class Yahtzee {
 						hand[dieNumber] = rollDie();
 					}
 				}
-				
+
 				System.out.print("Your roll was: ");
 				for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
 				{
 					System.out.print(hand[dieNumber] + " ");
 				}
 				System.out.print("\n");
-				
+
 				if(turn < 3)
 				{
 					System.out.print("enter dice to keep (y or n) ");
 					keep += scnr.nextLine();
 				}
-				turn++;	
+				turn++;
 			}
-			
+
 			sortArray(hand, DICE_IN_PLAY);
 			System.out.print("Here is your sorted hand: ");
 			for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
@@ -58,7 +58,7 @@ public class Yahtzee {
 				System.out.print(hand[dieNumber] + " ");
 			}
 			System.out.print("\n");
-			
+
 			for (int dieValue = 1; dieValue <=6; dieValue++)
 	        {
 	            int currentCount = 0;
@@ -76,59 +76,59 @@ public class Yahtzee {
 				System.out.print("3 of a Kind line\n");
 	        }
 	        else System.out.print("Score 0 on the 3 of a Kind line\n");
-	        
+
 	        if (maxOfAKindFound(hand) >= 4)
 	        {
 	        	System.out.print("Score " + totalAllDice(hand) + " on the ");
 	        	System.out.print("4 of a Kind line\n");
 	        }
 	        else System.out.print("Score 0 on the 4 of a Kind line\n");
-	        
+
 	        if (fullHouseFound(hand))
 	            System.out.print("Score 25 on the Full House line\n");
 	        else
 	            System.out.print("Score 0 on the Full House line\n");
-	        
+
 	        if (maxStraightFound(hand) >= 4)
 	            System.out.print("Score 30 on the Small Straight line\n");
 	        else
 	            System.out.print("Score 0 on the Small Straight line\n");
-	        
+
 	        if (maxStraightFound(hand) >= 5)
 	            System.out.print("Score 40 on the Large Straight line\n");
 	        else
 	            System.out.print("Score 0 on the Large Straight line\n");
-	        
+
 	        if (maxOfAKindFound(hand) >= 5)
 	            System.out.print("Score 50 on the Yahtzee line\n");
 	        else
 	            System.out.print("Score 0 on the Yahtzee line\n");
-	        
+
 	        System.out.print("Score " + totalAllDice(hand) + " on the ");
 	        System.out.print("Chance line");
 	        System.out.print("\nEnter 'y' to play again ");
-	        
+
 	        playAgain = scnr.nextLine().charAt(0);
 	    }
-		
+
 		scnr.close();
 	}
-	
+
 	public int rollDie()
 	{
 		int roll = (int) (Math.random() * 6) + 1;
 		return roll;
 	}
-	
+
 	public int maxOfAKindFound(int[] hand)
 	{
 		int maxCount = 0;
 		int currentCount;
-		
+
 		for(int dieValue = 1; dieValue <= 6; dieValue++)
 		{
 			currentCount = 0;
-			
+
 			for(int diePosition = 0; diePosition < 5; diePosition++)
 			{
 				if(hand[diePosition] == dieValue)
@@ -136,7 +136,7 @@ public class Yahtzee {
 					currentCount++;
 				}
 			}
-			
+
 			if(currentCount > maxCount)
 			{
 				maxCount = currentCount;
@@ -144,12 +144,12 @@ public class Yahtzee {
 		}
 		return maxCount;
 	}
-	
+
 	public int maxStraightFound(int[] hand)
 	{
 		int maxLength = 1;
 		int curLength = 1;
-		
+
 		for(int counter = 0; counter < 4; counter++)
 		{
 			if(hand[counter] + 1 == hand[counter + 1])
@@ -157,24 +157,24 @@ public class Yahtzee {
 				curLength++;
 			} else if(hand[counter] + 1 == hand[counter + 1]) {
 				curLength = 1;
-			} 
-			
+			}
+
 			if(curLength > maxLength)
 			{
 				maxLength = curLength;
 			}
 		}
-		
+
 		return maxLength;
 	}
-	
+
 	public boolean fullHouseFound(int[] hand)
 	{
 		boolean foundFH = false;
 		boolean found3K = false;
 		boolean found2K = false;
 		int currentCount;
-		
+
 		for(int dieValue = 1; dieValue <= 6; dieValue++)
 		{
 			currentCount = 0;
@@ -185,35 +185,35 @@ public class Yahtzee {
 					currentCount++;
 				}
 			}
-			
+
 			if(currentCount == 2)
 			{
 				found2K = true;
 			}
-			
+
 			if(currentCount == 3)
 			{
 				found3K = true;
 			}
 		}
-		
+
 		if(found2K && found3K)
 		{
 			foundFH = true;
 		}
-		
+
 		return foundFH;
 	}
-	
+
 	public int totalAllDice(int[] hand)
 	{
 		int total = 0;
-		
+
 		for(int diePosition = 0; diePosition < 5; diePosition++)
 		{
 			total += hand[diePosition];
 		}
-		
+
 		return total;
 	}
 
@@ -221,8 +221,8 @@ public class Yahtzee {
 	{
 		boolean swap;
 		int temp;
-		
-		do		
+
+		do
 		{
 			swap = false;
 			for(int count = 0; count < (size - 1); count++)
@@ -237,5 +237,5 @@ public class Yahtzee {
 			}
 		} while (swap);
 	}
-	
+
 }
