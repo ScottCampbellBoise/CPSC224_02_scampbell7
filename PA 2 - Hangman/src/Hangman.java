f10333beffe5961ef499f68c6254c12c7e0722fd
Scott <scottcampbell.boise@gmail.com>
2019-02-03 15:19:24 -0800

    Added random words and updated program

--- PA 2 - Hangman/src/Hangman.java
+++ PA 2 - Hangman/src/Hangman.java
@@ -1,4 +1,33 @@
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class Hangman {
 
+	public static void main(String[] args)
+	{
+		new Hangman();
+	}
+	
+	public Hangman()
+	{
+		displayMenu();
+	}
+	
+	// Displays 3 options - rand word, user-spec. word, exit 
+	public void displayMenu()
+	{
+		String[] buttons = {"Exit Game", "Enter your own Word", "Use Random Word"};
+
+	}
+	
+	public String getRandomWord()
+	{
+		return null;
+	}
+	
+	public String getWordFromUser()
+	{
+		return null;
+	}
+	
 }
