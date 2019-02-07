de808a3ec23dd661c4c5399878f987da78862e17
Scott <scottcampbell.boise@gmail.com>
2019-02-06 16:54:10 -0800

    Updated files for PA2

--- PA 2 - Hangman/src/Hangman.java
+++ PA 2 - Hangman/src/Hangman.java
@@ -8,12 +8,8 @@ public class Hangman {
 	
 	//TO DO:
 	/**
-	 * Crop and resize images
 	 * Check if word is complete
-	 * Check if word is too long
-	 * Check if entered char is a char and is valid/not already guessed
 	 * Way to end the game
-	 * Convert all inputed strings to lower case
 	 */
 	
 	final String filePath = "words.txt";
@@ -32,15 +28,19 @@ public class Hangman {
 		String guessedChars = "";
 		int numStrikes = 0;
 		
-		while(numStrikes <= 6)
+		while(numStrikes <= 6 && word != null)
 		{
-			char enteredChar = displayHangman(word,guessedChars, numStrikes);
-			
-			if(word.contains(""+enteredChar))
+			String enteredChar = displayHangman(word,guessedChars, numStrikes);
+			if(enteredChar != null)
 			{
 				guessedChars += enteredChar;
+				
+				if(!word.contains(""+enteredChar))
+					numStrikes++;
 			} else {
-				numStrikes++;
+				word = displayMenu();
+				guessedChars = "";
+				numStrikes = 0;
 			}
 			
 		}
@@ -107,22 +107,18 @@ public class Hangman {
 		return isValid;
 	}
 	
-	public char displayHangman(String word, String guessedChars, int numStrikes)
-	{
-		String guessedWord = "\t";
-		String title = 		"\t**Hang-man**\t";
-		String starHeader = "\t****************\t";
-		String dotHeader = 	"\t------------\t";
-		String iconPath = "strike"+numStrikes+".png";
+	public String displayHangman(String word, String guessedChars, int numStrikes)
+	{	
+		String guessedWord = 	"\t";
+		String title = 			"\t**Hang-man**\t";
+		String starHeader = 	"\t****************\t";
+		String dotHeader = 		"\t------------\t";
+		String guessedHeader = 	"\tUsed Letters\t";
+		String usedLetters = 	"\t";
+		String iconPath = "strike" + numStrikes + ".png";
 		
-		for(int k = 0; k < word.length(); k++)
-		{
-			if(guessedChars.contains(word.substring(k,k+1)))
-				guessedWord += " " + word.charAt(k) + " ";
-			 else 
-				 guessedWord += " _ "; 
-		}
-		guessedWord += "\t";
+		guessedWord = parseWordToDisplay(word, guessedWord, guessedChars);
+		usedLetters = parseUsedLettersToDisplay(word, usedLetters, guessedChars);
 		
 		if(guessedWord.length() < 20) //min length for headers
 		{
@@ -130,43 +126,76 @@ public class Hangman {
 			int leftPad = extra / 2;
 			int rightPad = extra - leftPad;
 			
-			for(int k = 0; k < leftPad; k++)
-			{
-				guessedWord = " " + guessedWord;
-			}
-			
-			for(int k = 0; k < rightPad; k++)
-			{
-				guessedWord += " ";
-			}
+			guessedWord = pad(guessedWord, leftPad, rightPad);
 		} else {
 			int extra = guessedWord.length() - 20;
 			int leftPad = extra / 2;
 			int rightPad = extra - leftPad;
 			
-			for(int k = 0; k < leftPad; k++)
-			{
-				 title = " " + title;
-				 starHeader = " " + starHeader;
-				 dotHeader = " " + dotHeader;
-			}
-			
-			for(int k = 0; k < rightPad; k++)
-			{
-				title += " ";
-				starHeader += " ";
-				dotHeader += " ";
-			}
+			title = pad(title, leftPad, rightPad);
+			starHeader = pad(starHeader, leftPad, rightPad);
+			dotHeader = pad(dotHeader, leftPad, rightPad);
+			usedLetters = pad(usedLetters, leftPad, rightPad);
+			guessedHeader = pad(guessedHeader, leftPad, rightPad);
 		}
 		
-		String message = title + "\n" + starHeader + "\n" + dotHeader + "\n" + guessedWord + "\n" + dotHeader + "\n" + starHeader;
+		if(numStrikes < 6)
+		{
+			String message = title + "\n" + starHeader + "\n" + dotHeader + "\n" + guessedWord + "\n" + dotHeader + "\n\n" + guessedHeader + "\n" + usedLetters + "\n" + starHeader;
+			String response = (String) JOptionPane.showInputDialog(null, message, "Hang Man", 0, new ImageIcon(iconPath), null, null);
+			
+			while(response != null && response.length() > 1 && !checkWord(response))
+			{
+				response = JOptionPane.showInputDialog(null, "Please enter a single valid character!");
+			}
 		
-		String response = (String) JOptionPane.showInputDialog(null, message, "Title", 0, new ImageIcon(iconPath), null, null);
+			return response;
+		} else { //Striked out
+			String message = title + "\n" + starHeader + "\n" + dotHeader + "\n" + word + "\n" + dotHeader + "\n\n" + guessedHeader + "\n" + usedLetters + "\n" + starHeader;
+			JOptionPane.showMessageDialog(null, message, "You Struck Out!", JOptionPane.INFORMATION_MESSAGE, new ImageIcon("strike6.png"));
+			return null;
+		}
+	}
+	
+	String parseUsedLettersToDisplay(String word, String usedLetters, String guessedChars)
+	{
+		for(int k = 0; k < guessedChars.length(); k++)
+		{
+			if(!word.contains(guessedChars.substring(k,k+1)))
+			{
+				usedLetters += " " + guessedChars.charAt(k) + " ";
+			}
+		}
+		return (usedLetters + "\t");
+	}
+	
+	String parseWordToDisplay(String word, String guessedWord, String guessedChars)
+	{
+		for(int k = 0; k < word.length(); k++)
+		{
+			if(guessedChars.contains(word.substring(k,k+1)))
+			{
+				guessedWord += " " + word.charAt(k) + " ";
+			} else {
+				 guessedWord += " _ "; 
+			}
+			
+		}
+		return (guessedWord + "\t");
+	}
+	
+	String pad(String original, int leftPad, int rightPad)
+	{
+		for(int k = 0; k < leftPad; k++)
+		{
+			original = " " + original;
+		}
 		
-		//MAKE CHECK FOR THE CHARACTER RETURNED
-		//CHECK IF WORD IS COMPLETE
-		
-		return response.charAt(0);
+		for(int k = 0; k < rightPad; k++)
+		{
+			original += " ";
+		}
+		return original;
 	}
 	
 }
