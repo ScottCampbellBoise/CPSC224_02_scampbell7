0de2f35bfb950e269072f0942fc991d5c45f65e6
Scott <scottcampbell.boise@gmail.com>
2019-03-24 21:52:49 -0700

    Updated PDF Creation Functionality

---
+++ GlazeCatalog V2/src/PDF_Generator_v2.java
@@ -0,0 +1,71 @@
+import java.awt.Color;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Anchor;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.List;
+import com.itextpdf.text.ListItem;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import com.itextpdf.io.image.ImageData; 
+import com.itextpdf.io.image.ImageDataFactory; 
+
+import com.itextpdf.kernel.pdf.PdfDocument; 
+
+import com.itextpdf.text.Image;
+
+public class PDF_Generator_v2 {
+
+	
+	public static void main(String[] args)
+	{
+		new PDF_Generator_v2();
+		System.out.println("Finished ...");
+	}
+	
+	public PDF_Generator_v2()
+	{
+		try{
+			createGlazePage();
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR !!!!");
+		}
+	}
+	
+	public void createGlazePage() throws Exception
+	{
+		Document document = new Document();
+	    //Create new File
+	    File file = new File("/Users/ScottCampbell/Desktop/testPDF.pdf");
+	    file.createNewFile();
+	    FileOutputStream fop = new FileOutputStream(file);
+	    PdfWriter.getInstance(document, fop);
+	    document.open(); 
+	    
+	    Font fontHeader = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+	    Font fontBody = new Font(Font.FontFamily.COURIER, 12, Font.NORMAL);
+	    
+	    
+	    Image image1 = Image.getInstance("/Users/ScottCampbell/Desktop/addy.jpg");
+	    image1.scaleAbsolute(450, 250);
+	    //Add to document
+	    document.add(image1);
+
+	    
+	    document.close();	    
+	}
+	
+}
